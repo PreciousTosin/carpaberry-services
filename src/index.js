ed26c672279403d7a5649d71d0cae2a8b5040ff1
precioustosin <precioustosin@hotmail.com>
2018-08-01 23:54:41 +0100

    add logic to toggle sidebar

--- src/index.js
+++ src/index.js
@@ -9,4 +9,11 @@ const $ = require('jquery');
 
 $(document).ready(() => {
   console.log('Place JavaScript code here and there and over threre...');
+  document.querySelector('#menu').addEventListener('click', (e) => {
+    console.log('ICON CLICKED!');
+    document.querySelector('#sidenav').classList.toggle('open');
+    document.querySelector('#list').classList.toggle('disable--list');
+    document.querySelector('#drawer').classList.toggle('shift--brand');
+    e.stopPropagation();
+  });
 });
