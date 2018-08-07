cebd5fda33f29cc551d9fabafbf6d07472a9d2df
precioustosin <precioustosin@hotmail.com>
2018-08-07 03:00:59 +0100

    created functions on the cliend side to calculate costs of orders

--- src/index.js
+++ src/index.js
@@ -8,6 +8,32 @@ import '../public/css/main-style.css';
 
 const $ = require('jquery');
 
+function calcCost(payload) {
+  const regularWear = payload.regularWear * 40;
+  const underWear = payload.underWear * 20;
+  const largeItems = payload.largeItems * 100;
+  const totalCost = regularWear + underWear + largeItems;
+  return {
+    regularWear,
+    underWear,
+    largeItems,
+    totalCost
+  };
+}
+
+function handleSummaryCost() {
+  const payload = {
+    regularWear: document.querySelector('#regWear').value,
+    underWear: document.querySelector('#undWear').value,
+    largeItems: document.querySelector('#lagItems').value,
+  };
+  const costObj = calcCost(payload);
+  document.querySelector('#regWearCost').value = costObj.regularWear;
+  document.querySelector('#undWearCost').value = costObj.underWear;
+  document.querySelector('#lagItemsCost').value = costObj.largeItems;
+  document.querySelector('#totalCost').value = costObj.totalCost;
+}
+
 $(document).ready(() => {
   console.log('Place JavaScript code here and there and over threre...');
   document.querySelector('#menu').addEventListener('click', (e) => {
@@ -16,5 +42,13 @@ $(document).ready(() => {
     document.querySelector('#list').classList.toggle('disable--list');
     document.querySelector('#drawer').classList.toggle('shift--brand');
     e.stopPropagation();
+  });
+  const inputElement = document.querySelectorAll('.cloth--qty');
+  inputElement.forEach((input) => {
+    input.addEventListener('focusout', (event) => {
+      console.log('focusout event fired');
+      handleSummaryCost();
+      event.stopPropagation();
+    });
   });
 });
