60fee35c72c1fa81cc14f6204a3241b4f50c8e9b
precioustosin <precioustosin@hotmail.com>
2018-08-10 17:42:50 +0100

    added more fields to the transaction model

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
+  transactionRef: String,
   amount: Number,
   payMethod: String,
+  currency: String,
 }, { timestamps: true });
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
-module.exports = { Transaction, transactionSchema };
+module.exports = Transaction;
